6a8656b3f09d4423f573b90e3e5d8cd56f7dd44d
anragam <87624412+anragam@users.noreply.github.com>
2021-07-19 11:04:29 +0900

    Add files via upload

---
+++ web02/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $("#Wrap").fullpage({
+        navigation: true,
+        afterLoad: function(origin, destination, direction){
+           $(".section").eq(destination.index).addClass("on").siblings().removeClass("on");
+        },
+    });
+
+
+
+});
